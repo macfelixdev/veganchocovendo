6fd6f3e955ea1bf9f66d4bc74699c9c757bea6f5
macfelixdev <mac.felix.dev@gmail.com>
2019-04-18 22:29:37 +0800

    Updates

---
+++ precache-manifest.e59d04569327309c6d23ba914ddf8e49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4090c1ff7f40d7ec0cfc",
+    "url": "/veganchocovendo/static/js/runtime~main.38be3416.js"
+  },
+  {
+    "revision": "ec55607c9f3041fb4531",
+    "url": "/veganchocovendo/static/js/main.80c1fd03.chunk.js"
+  },
+  {
+    "revision": "e1df9e5dd97f9dc5b41a",
+    "url": "/veganchocovendo/static/js/2.373ccd69.chunk.js"
+  },
+  {
+    "revision": "ec55607c9f3041fb4531",
+    "url": "/veganchocovendo/static/css/main.27ff581b.chunk.css"
+  },
+  {
+    "revision": "49eb0ba5cb19f7df82e4b730522048b9",
+    "url": "/veganchocovendo/index.html"
+  }
+];
